031c037095f1b358302901fd1582bd4ed958406b
johnsonpeter-j <johnson.jp@myyahoo.com>
2025-07-23 10:17:53 +0530

    sidebar added

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,15 @@
+import { AppSidebar } from "@/components/sidebar";
+import { SidebarProvider } from "@/components/ui/sidebar";
+
 export default function Home() {
   return (
     <div className="font-sans grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
-      Welcome
+      <SidebarProvider >
+        <AppSidebar />
+        </SidebarProvider>
+      <div>
+        Welcome
+      </div>
     </div>
   );
 }
